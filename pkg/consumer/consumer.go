7754941c5a5db33b6ff1f498e61b1ef81509538a
Ali Shanaakh <github@shal.dev>
2019-09-06 16:07:45 +0300

    Refactor logging

--- pkg/consumer/consumer.go
+++ pkg/consumer/consumer.go
@@ -4,22 +4,16 @@ import (
 	"bytes"
 	"fmt"
 	"io/ioutil"
-	"os"
 	"strings"
 
 	"github.com/antonmedv/expr"
 	"github.com/go-yaml/yaml"
-	"github.com/rs/zerolog"
-	"github.com/rs/zerolog/log"
 
 	"github.com/openware/postmaster/internal/config"
+	"github.com/openware/postmaster/internal/log"
 	"github.com/openware/postmaster/pkg/amqp"
 	"github.com/openware/postmaster/pkg/env"
 	"github.com/openware/postmaster/pkg/eventapi"
-)
-
-var (
-	Logger = log.Logger
 )
 
 func amqpURI() string {
@@ -36,66 +30,37 @@ func requireEnvs() {
 	env.Must(env.Fetch("SENDER_EMAIL"))
 }
 
-// Logger sets settings of zerolog logger.
-// Supported environment variables:
-// - POSTMASTER_ENV can be either "development" or "production".
-// - POSTMASTER_LOG_LEVEL can be "debug", "info", "warn", "error", "fatal", "panic". (default: "debug")
-func configureLogger() {
-	logLevel, ok := os.LookupEnv("POSTMASTER_LOG_LEVEL")
-	if ok {
-		level, err := zerolog.ParseLevel(logLevel)
-		if err != nil {
-			log.Fatal().Err(err)
-		}
-
-		zerolog.SetGlobalLevel(level)
-		return
-	}
-
-	environ, ok := os.LookupEnv("POSTMASTER_ENV")
-	if strings.EqualFold("production", environ) {
-		zerolog.SetGlobalLevel(zerolog.WarnLevel)
-	} else {
-		zerolog.SetGlobalLevel(zerolog.DebugLevel)
-		Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
-	}
-
-	env.Logger = Logger
-}
-
 // Run starts the application.
 func Run(path, tag string) {
-	configureLogger()
 	requireEnvs()
 
 	conf := new(config.Config)
 	content, err := ioutil.ReadFile(path)
 	if err != nil {
-		Logger.Fatal().Err(err).
+		log.Fatal().Err(err).
 			Msgf("can not read file %s", path)
 	}
 
 	if err := yaml.Unmarshal([]byte(content), &conf); err != nil {
-		Logger.Fatal().Err(err).
+		log.Fatal().Err(err).
 			Msgf("can not unmarshal configuration %s", path)
 	}
 
 	if err := conf.Validate(); err != nil {
-		Logger.Fatal().Err(err).
+		log.Fatal().Err(err).
 			Msgf("configuration file %s is not valid", path)
 	}
 
 	serveMux := amqp.NewServeMux(amqpURI(), tag, conf.Exchanges, conf.Keychain)
-	serveMux.Logger = Logger
 
 	for id := range conf.Events {
 		eventConf := conf.Events[id]
 		serveMux.HandleFunc(eventConf.Key, eventConf.Exchange, func(raw eventapi.RawEvent) {
-			Logger.Info().Msgf("processing event %s", eventConf.Key)
+			log.Info().Msgf("processing event %s", eventConf.Key)
 
 			event, err := eventapi.Unmarshal(raw)
 			if err != nil {
-				Logger.Error().
+				log.Error().
 					Err(err).
 					Fields(raw).
 					Msg("can not unmarshal event")
@@ -104,15 +69,15 @@ func Run(path, tag string) {
 
 			record, err := event.FixAndValidate(conf.Languages[0].Code)
 			if err != nil {
-				Logger.Error().Err(err).Fields(raw).Msg("event is not valid")
+				log.Error().Err(err).Fields(raw).Msg("event is not valid")
 				return
 			}
 
-			Logger.Info().Str("uid", record.User.UID).Str("email", record.User.Email).Msgf("event received")
+			log.Info().Str("uid", record.User.UID).Str("email", record.User.Email).Msgf("event received")
 
 			// Checks, that language is supported.
 			if !conf.ContainsLanguage(record.Language) {
-				Logger.Error().
+				log.Error().
 					Str("language", record.Language).
 					Msg("language is not supported")
 				return
@@ -121,32 +86,32 @@ func Run(path, tag string) {
 			if strings.TrimSpace(eventConf.Expression) != "" {
 				result, err := expr.Eval(eventConf.Expression, raw)
 				if err != nil {
-					Logger.Error().Err(err).Msg("expression evaluation failed")
+					log.Error().Err(err).Msg("expression evaluation failed")
 				}
 
 				match, ok := result.(bool)
 				if !ok {
-					Logger.Error().Err(err).Msg("expression result is not boolean")
+					log.Error().Err(err).Msg("expression result is not boolean")
 					return
 				}
 
-				logger := Logger.Info().
+				log := log.Info().
 					Str("uid", record.User.UID).
 					Str("email", record.User.Email).
 					Interface("match", result)
 
 				if !match {
-					logger.Msgf("skipped")
+					log.Msgf("skipped")
 					return
 				}
 
-				logger.Msgf("matched")
+				log.Msgf("matched")
 			}
 
 			tpl := eventConf.Template(record.Language)
 			content, err := tpl.Content(raw)
 			if err != nil {
-				Logger.Error().Err(err).Msg("template execution failed")
+				log.Error().Err(err).Msg("template execution failed")
 				return
 			}
 
@@ -167,14 +132,14 @@ func Run(path, tag string) {
 			}
 
 			if err := NewEmailSender(conf, email).Send(); err != nil {
-				Logger.Error().Err(err).Msg("failed to send email")
+				log.Error().Err(err).Msg("failed to send email")
 				return
 			}
 		})
 	}
 
-	Logger.Info().Msg("waiting for events")
+	log.Info().Msg("waiting for events")
 	if err := serveMux.ListenAndServe(); err != nil {
-		Logger.Panic().Err(err).Msg("connection failed")
+		log.Panic().Err(err).Msg("connection failed")
 	}
 }
